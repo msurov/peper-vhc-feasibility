048734bc2b523c8b1e66b5602e5c6b0653840d78
Maksim Surov <msurov>
2025-12-31 19:12:44 +0300

    temp change

--- make.sh
+++ make.sh
@@ -2,7 +2,8 @@
 
 set -e
 mkdir -p build
+cd tex
 # rm build/vhc-feasibility.bbl
-texfot pdflatex -interaction=nonstopmode -file-line-error -output-directory=build vhc-feasibility.tex
-texfot bibtex build/vhc-feasibility
-texfot pdflatex -interaction=nonstopmode -file-line-error -output-directory=build vhc-feasibility.tex
+texfot pdflatex -interaction=nonstopmode -file-line-error -output-directory=../build vhc-feasibility.tex
+texfot bibtex ../build/vhc-feasibility
+texfot pdflatex -interaction=nonstopmode -file-line-error -output-directory=../build vhc-feasibility.tex
